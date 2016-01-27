3ac25595152b6ff16d01d311f48f2cd2e1225d1b
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2016-01-27 22:54:32 +0000

    Mini bibliography tool

---
+++ minibib.ml
@@ -0,0 +1,70 @@
+(* A very basic datatype for a bibliographic entry. *)
+type entry =
+  { key     : string
+  ; authors : string
+  ; title   : string
+  ; notes   : string
+  ; year    : string }
+
+type bibliography =
+  (string * (entry * bool ref)) list
+
+(* Low level storage of the bibliography. *)
+module File =
+  struct
+    let write : string -> bibliography -> unit = fun fn b ->
+      let oc = open_out fn in
+      output_value oc b;
+      close_out oc
+
+    let read : string -> bibliography = fun fn ->
+      let ic = open_in fn in
+      let b = input_value ic in
+      close_in ic; b
+
+    let create : string -> unit = fun fn ->
+      write fn []
+
+    let insert : string -> entry -> unit = fun fn e ->
+      let b = read fn in
+      if List.mem_assoc e.key b then
+        Printf.eprintf "[Minibib] Conflicting key %S.\n%!" e.key
+      else
+        write fn ((e.key, (e, ref false)) :: b)
+
+    let touch : string -> string -> unit = fun fn key ->
+      let b = read fn in
+      try
+        let (_,r) = List.assoc key b in
+        r := true; write fn b;
+      with Not_found ->
+        Printf.eprintf "[Minibib] Not found %S.\n%!" key
+  end
+
+type biblio = string
+
+let create : string -> biblio =
+  fun name ->
+    name ^ ".minibib" 
+
+let insert : entry -> biblio -> unit =
+  fun e bib ->
+    File.insert bib e
+
+let insert_many : entry list -> biblio -> unit =
+  fun es bib ->
+    List.iter (fun e -> insert e bib) es
+
+let entries : biblio -> entry list =
+  fun bib ->
+    let es = File.read bib in
+    let es = List.filter (fun (_,(_,u)) -> !u) es in
+    let es = List.map (fun (_,(e,_)) -> e) es in
+    List.sort (fun e1 e2 -> String.compare e1.key e2.key) es
+
+let cite : biblio -> (string list -> 'a) -> string list -> 'a =
+  fun bib act ks ->
+    if List.length ks < 1 then
+      Printf.eprintf "[Minibib] No keys provided.\n%!";
+    List.iter (File.touch bib) ks;
+    act ks
---
+++ minibib.mli
@@ -0,0 +1,33 @@
+(* Type of a bibliography entry. *)
+type entry =
+  { key     : string   (* Key used for citation. *)
+  ; authors : string   (* Authors. *)
+  ; title   : string   (* Title. *)
+  ; notes   : string   (* Additional data (e.g. publisher). *)
+  ; year    : string } (* Year. *)
+
+(* Abstract bibliography type. *)
+type biblio
+
+(* [create name] creates a new bibliography with the name [name]. The name
+   sould be unique for a given document. Unspecified behaviours are to be
+   expected otherwise. *)
+val create : string -> biblio
+
+(* [insert e b] inserts the entry [e] in the bibliography [b]. If the key
+   for the entry is already used, a warning message is displayed. In this
+   case, the given entry is not inserted. *)
+val insert : entry -> biblio -> unit
+
+(* Similar to [insert] with several different entries at once. *)
+val insert_many : entry list -> biblio -> unit
+
+(* [entries b] produces a list of entries sorted by author name. Only the
+   entries that were previously referenced using [cite] or [mcite] are in
+   this list. *)
+val entries : biblio -> entry list
+
+(* [cite bib act ks] produces a reference to the entries of bibliography
+   [b] having their key listed in [ks]. The function [act] is used to
+   actually produce a reference to display. *)
+val cite : biblio -> (string list -> 'a) -> string list -> 'a
