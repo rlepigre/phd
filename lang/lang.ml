e46c7878b172fe753a46fe3bf372bf59c67d90b7
rodolphe.lepigre <rodolphe.lepigre@univ-savoie.fr>
2016-10-25 09:07:16 +0000

    Caching the local parsers

--- lang/lang.ml
+++ lang/lang.ml
@@ -3,19 +3,28 @@ open Lang_parser
 
 type maths = Maths.math list
 
-let v : string -> maths = fun s -> v2m (parse_valu s)
-let t : string -> maths = fun s -> t2m (parse_term s)
-let c : string -> maths = fun s -> c2m (parse_ctxt s)
-let s : string -> maths = fun s -> s2m (parse_stac s)
-let p : string -> maths = fun s -> p2m (parse_proc s)
-let f : string -> maths = fun s -> f2m (parse_form s)
+let with_cache : (string -> maths) -> string -> maths =
+  let cache = Hashtbl.create 101 in
+  let with_cache f s =
+    try Hashtbl.find cache s with Not_found ->
+      let res = f s in
+      Hashtbl.add cache s res; res
+  in with_cache
 
-let subs : string -> maths = fun s -> subs2m (parse_subs s)
+let v : string -> maths = with_cache (fun s -> v2m (parse_valu s))
+let t : string -> maths = with_cache (fun s -> t2m (parse_term s))
+let c : string -> maths = with_cache (fun s -> c2m (parse_ctxt s))
+let s : string -> maths = with_cache (fun s -> s2m (parse_stac s))
+let p : string -> maths = with_cache (fun s -> p2m (parse_proc s))
+let f : string -> maths = with_cache (fun s -> f2m (parse_form s))
+
+let subs : string -> maths = with_cache (fun s -> subs2m (parse_subs s))
 
 (*
 let debug name f s =
-  Printf.printf "Parsing \"%s\" as a %s.\n%!" s name;
-  f s
+  Printf.printf "Parsing \"%s\" as a %s ... %!" s name;
+  let res = f s in
+  Printf.printf "Done.\n%!"; res
 
 let v = debug "value"   v
 let t = debug "term"    t
